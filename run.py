f241c868ddc9f69062e2a8134dd4eaf65bf6d9e4
gloz0315 <80665963+gloz0315@users.noreply.github.com>
2021-11-13 21:21:41 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
-from Class_func2 import Class_func2
+from Class_func2 import Class_func2  # 하위폴더 Class_func2 로부터 클래스  Class_func2를 import함
 
-inst_1 = Class_func2()
-a = Class_func2()
-a.function_2(10,20)
+a = Class_func2() # 인스턴스 생성
+a.function_2(10,20) # 메소드 호출
