58908fd088f0c2024b787cfc36afd45cd317102d
gloz0315 <80665963+gloz0315@users.noreply.github.com>
2021-11-18 18:20:28 +0900

    Update Calc_valid(1).py

--- week7/Calc_valid(1).py
+++ week7/Calc_valid(1).py
@@ -4,21 +4,16 @@ class Calc_valid:
         self.num2 = num2
         self.flag = flag
 
-
     def is_valid_clac(self, ):
 
-        self.flag = True
+        self.flag = False
 
         try:
-            self.num1/self.num2
-            self.num2/self.num1
+            self.num1 / self.num2
+            self.num2 / self.num1
         except ZeroDivisionError:
-
-            self.flag = False
             print("계산 불가능한 값입니다.")
         except ZeroDivisionError:
-
-            self.flag = False
             print("계산 불가능한 값입니다.")
         else:
             self.flag = True
