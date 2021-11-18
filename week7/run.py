d34fe6b995681188a23f3f922a53590a1274abbc
gloz0315 <80665963+gloz0315@users.noreply.github.com>
2021-11-18 18:12:36 +0900

    Update run.py

--- week7/run.py
+++ week7/run.py
@@ -3,3 +3,4 @@ from Calc_valid import Calc_valid
 a = True                     # flag는 주로 전역변수로 사용.
 inst_1 = Calc_valid(30,10,a) # 인스턴스 생성 num1 과 num2에 30과 10을 넣음
 inst_1.is_valid_clac()
+print(f"{inst_1.flag}")
