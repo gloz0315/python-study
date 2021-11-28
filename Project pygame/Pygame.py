60e2aabf2f7ae747066ac8e148c8583b0b4ba70e
gloz0315 <80665963+gloz0315@users.noreply.github.com>
2021-11-28 19:32:55 +0900

    Create Pygame.py

---
+++ Project pygame/Pygame.py
@@ -0,0 +1,29 @@
+import pygame
+
+pygame.init() # 파이게임 초기화
+
+# 윈도우 창 설정하기
+WINDOW_WIDTH = 800 # 가로 크기
+WINDOW_HIGHT = 600 # 세로 크기
+
+surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HIGHT)) # 화면 설정
+
+pygame.display.set_caption("지렁이 게임") # 게임 이름
+background = pygame.image.load("C:/Users/User/PycharmProjects/study/project pygame/image/image1.png.jpg")
+surface.blit(background, (0,0)) # 배경 그리기 (0,0)은 background가 표시되는 위치
+done = False
+
+def run():
+    global done
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+                break
+
+        surface.blit(background, (0, 0))  # 배경 그리기 (0,0)은 background가 표시되는 위치
+        pygame.display.update() #화면 그리기 업데이트
+
+run() # 게임 실행
+pygame.quit() # 나가기
+
